916eb3c120be49f888368c829734f82d42e167b0
khaidongduc <khaidongduc@gmail.com>
2020-11-30 21:35:39 +0700

    change eval.py to plot the first 25 predictions

--- eval.py
+++ eval.py
@@ -13,10 +13,15 @@ model.load_weights("model/trained_model.ckpt")
 # eval
 model.evaluate(x_test, y_test)
 
-# show false predictions
+# show 25 first predictions
 predictions = np.argmax(model.predict(x_test), axis=1)
-for i, prediction in enumerate(predictions):
-    if prediction != y_test[i]:
-        plt.title("Expected: {}, Actual: {}".format(y_test[i], prediction))
-        plt.imshow(x_test[i], cmap="gray")
-        plt.show()
+plt.figure(figsize=(10, 10))
+for i in range(25):
+    img = x_test[i]
+    plt.subplot(5, 5, i + 1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(img, cmap="gray")
+    plt.xlabel("E: {}, A: {}".format(y_test[i], predictions[i]))
+plt.show()
